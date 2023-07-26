2ff9c1072575e400af8d949c26fdf40e15be2975
Brian <b10902085@ntu.edu.tw>
2023-07-26 09:42:49 +0800

    add border

--- game.js
+++ game.js
@@ -2,29 +2,32 @@ document.querySelector('#imageInput').addEventListener('change', update);
 var CX=document.querySelector('#cx'), CY=document.querySelector('#cy'), R=document.querySelector('#radius');
 CX.value=300, CY.value=300, R.value=200;
 var cx=Number(CX.value), cy=Number(CY.value), r=Number(R.value);
+var w1=1000, h1=1000;
 
-function inversion(cx, cy, r){
+function f(x, y){
+	var d=(x-cx)*(x-cx)+(y-cy)*(y-cy);
+	if(d==0)return [];
+	d=r*r/d;
+	return [Math.round(cx+(x-cx)*d), Math.round(cy+(y-cy)*d)];
+}
+
+function inversion(){
 	var cv0=document.querySelector('#original'), ct0=cv0.getContext('2d');
 	var cv1=document.querySelector('#inversion'), ct1=cv1.getContext('2d');
-	cv1.height=cv0.width, cv1.width=cv0.height;
-	var origin=ct0.getImageData(0, 0, cv0.width, cv0.height), inv=[];
-	for(var i=0; i<origin.data.length; ++i)inv.push(0);
 	var w0=cv0.width, h0=cv0.height;
-	var w1=h0, h1=w0;
-	for(var i=0; i<h0; ++i){
-		for(var j=0; j<w0; ++j){
-			var d=(cx-i)*(cx-i)+(cy-j)*(cy-j);
-			if(d==0)continue;
-			d=r*r/((cx-i)*(cx-i)+(cy-j)*(cy-j));
-			var x0=Math.round(cx+(i-cx)*d), y0=Math.round(cy+(j-cy)*d);
-			if(0<=x0&&x0<h0&&0<=y0&&y0<w0)for(var k=0; k<4; ++k)inv[4*(i*w1+j)+k]=origin.data[4*(x0*w0+y0)+k];
-		}
+	cv1.width=w1, cv1.height=h1;
+	var origin=ct0.getImageData(0, 0, cv0.width, cv0.height), inv=[];
+	for(var i=0; i<w1*h1*4; ++i)inv.push(0);
+	for(var i=0; i<h1; ++i)for(var j=0; j<w1; ++j){
+		var ret=f(cx-h1/2+i, cy-w1/2+j);
+		if(ret.length==0)continue;
+		x0=ret[0], y0=ret[1];
+		if(0<=x0&&x0<h0&&0<=y0&&y0<w0)for(var k=0; k<4; ++k)inv[4*(i*w1+j)+k]=origin.data[4*(x0*w0+y0)+k];
 	}
 	var inverse=ct1.getImageData(0, 0, cv1.width, cv1.height);
 	for(var i=0; i<inv.length; ++i)inverse.data[i]=inv[i];
 	ct1.putImageData(inverse, 0, 0);
 	document.querySelector('#text1').innerHTML='反演皮計算完成（在網頁的最下面）！';
-	alert('反演皮計算完成（在網頁的最下面）！');
 }
 
 function update(evt){
@@ -52,6 +55,27 @@ function upd(){
 	cx=Number(CX.value), cy=Number(CY.value), r=Number(R.value);
 	ct1.arc(cy, cx, r, 0, 2*Math.PI);
 	ct1.stroke();
+
+	var ret=f(cx-h1/2, cy-w1/2);
+	ct1.moveTo(ret[0], ret[1]);
+	ct1.beginPath();
+	for(var i=-h1/2, j=-w1/2; i<h1/2; ++i){
+		var ret=f(cx+i, cy+j);
+		ct1.lineTo(ret[0], ret[1]);
+	}
+	for(var i=h1/2, j=-w1/2; j<w1/2; ++j){
+		var ret=f(cx+i, cy+j);
+		ct1.lineTo(ret[0], ret[1]);
+	}
+	for(var i=h1/2, j=w1/2; i>=-h1/2; --i){
+		var ret=f(cx+i, cy+j);
+		ct1.lineTo(ret[0], ret[1]);
+	}
+	for(var i=-h1/2, j=w1/2; j>=-w1/2; --j){
+		var ret=f(cx+i, cy+j);
+		ct1.lineTo(ret[0], ret[1]);
+	}
+	ct1.strokeStyle='red', ct1.stroke();
 }
 
 function inverse(){
